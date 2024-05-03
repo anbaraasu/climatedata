48e4b30d9f3c781165bece76ec15cd17ccb5ffc1
anbarasuv_hcl <LP-PC226RTG@dnapass.com>
2024-05-03 10:40:28 +0530

    Without github copilot suggestion

--- ClimateService/src/test/java/com/sme/climatedata/ClimateControllerTest.java
+++ ClimateService/src/test/java/com/sme/climatedata/ClimateControllerTest.java
@@ -20,34 +20,6 @@ import com.sme.climatedata.service.ClimateService;
 @ExtendWith(MockitoExtension.class)
 public class ClimateControllerTest {
 
-    @InjectMocks
-    private ClimateController climateController;
-
-    @Mock
-    private ClimateService climateService;
-
-    @Test
-    public void testGetAllClimateData() {
-        // Arrange
-        ClimateData climateData = new ClimateData();
-        climateData.setCity("London");
-        climateData.setTemperature(20.0f);
-        climateData.setHumidity(50.0f);
-        climateData.setPrecipitation(0.0f);
-
-        when(climateService.getAllClimateData()).thenReturn(Arrays.asList(climateData));
-
-        // Act
-        ResponseEntity<List<ClimateData>> response = climateController.getAllClimateData();
-        List<ClimateData> result = response.getBody();
-
-        // Assert
-        assertEquals(1, result.size());
-        assertEquals("London", result.get(0).getCity());
-        assertEquals(20.0f, result.get(0).getTemperature());
-        assertEquals(50.0f, result.get(0).getHumidity());
-        assertEquals(0.0f, result.get(0).getPrecipitation());
-    }
 
     
 }
