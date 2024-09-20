aa9f95c3651ea2da23f1bff710d45dff1c57059c
anbarasuv_hcl <LP-PC226RTG@dnapass.com>
2024-09-20 18:17:09 +0530

    Refactor calculator code and update comments

--- calculator/CalcMain.java
+++
@@ -1,1 +0,0 @@
-//Calculator java app with menu options add, substract, division, multiplication and exit. loop until exit is the choice, exception handling for invalid inputs
---
+++ calculator/SME_Calc_Main.java
@@ -0,0 +1,1 @@
+// Calculator java app with menu options add, substract, division, multiplication and exit. Loop until exit is the choice, exception handling for invalid inputs.
