fefc0d556230d2afd87c315bdd833474f0e5d6c9
Anbu <anbarasuv@hcltech.com>
2024-12-06 09:51:13 +0530

    Remove CarApp and add CarDetailsApp with enhanced functionality for managing car details and improved exception handling

---
+++ carapp/CarDetailsApp.java
@@ -0,0 +1,2 @@
+// Car App - Java Console Application with menu to add, delete, update, search, list car details. Car details consist of name, model, company, price, color. Handling exceptions and validations.
+
