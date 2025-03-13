5af1184c7def71c9cb96a9177f1865964daeaa6e
Anbu <anbarasuv@hcltech.com>
2025-03-13 09:29:04 +0530

    feat: Implement Car Console Application and add Order Management Portal HTML

--- carapp/CarApp.java
+++ carapp/CarApp.java
@@ -1,4 +1,2 @@
 // Code Car App - Java Console Application with menu to add, delete, update, search, list car details. Car details consist of name, model, company, price, color. Handling exceptions and validations.
 
-
-
